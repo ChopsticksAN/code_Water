08b9343ee876c6f2b3c4862f1610a513aed36b4f
jinyu.wang <wwang_jy@sina.com>
2018-11-26 21:46:08 +0800

    update cleaningScript.py

--- Annex3/cleaningScript.py
+++ Annex3/cleaningScript.py
@@ -2,45 +2,93 @@ import docx
 import xlwt
 from docx import Document
 
-path = "C:\\Users\\王金宇\\Desktop\\数学建模\\111.doc"
-document = Document(path)
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText=[]
+    for i in doc.paragraphs:
+        fullText.append(i.text)
+    return fullText
 
-'''
-for paragraph in document.paragraphs:
-    print(paragraph.text)
-'''
 
-workbook = xlwt.Workbook(encoding='ascii')
-worksheet = workbook.add_sheet('My_Worksheet')
+def cleanFulltext(fullText):
+    for i in range(len(fullText), -1, -1):
+        if i >= len(fullText):
+            continue
+        elif len(fullText[i]) < 6:
+            fullText.remove(fullText[i])
+        elif fullText[i][5] != '2':
+            fullText.remove((fullText[i]))
 
-tables = document.tables
-t = len(tables)
-print(t)
+    for i in range(0, len(fullText)):
+        fullText[i] = fullText[i][5:]
+    return fullText
 
-rows = 0
-cols = 0
+def writeEndTable(end_table):
 
-for p in range(0, t):
-    table = tables[p]
-    for i in range(1, len(table.rows)):
-        #result = ""
-        cols = 0
-        for j in range(3, 9):
-            s = table.cell(i, j).text
-            #print(s);
-            worksheet.write(rows, cols, label=s)
-            cols += 1
+    return
+
+def main():
+    path = ".\\origin.doc"
+    document = Document(path)
+    fullText = getText(path)
+    fullText = cleanFulltext(fullText)
+
+    #print(len(fullText))
+
+    #for i in range(0, len(fullText)):
+    #    print(fullText[i][5:])
+
+    workbook = xlwt.Workbook(encoding='ascii')
+    worksheet = workbook.add_sheet('CleanData')
+
+    tables = document.tables
+    t = len(tables)
+    print(t)
+
+    rows = 0
+    cols = 0
+    ele = 0
+
+    #print(fullText[0])
+    #print(len(fullText[0]))
+    for j in range(3, 9):
+        s = tables[0].cell(1, j).text
+        worksheet.write(rows, cols, label=s)
+        cols += 1
+
+    worksheet.write(rows, cols, label="水流量")
+    worksheet.write(rows, cols + 1, label="水流速")
+
+    cols = 0
+    rows += 2
+    for p in range(0, t - 1):
+        worksheet.write(rows, cols, label=fullText[ele])
         rows += 1
-    rows += 1
+        ele += 1
+        table = tables[p]
+        for i in range(2, len(table.rows)):
+            cols = 0
+            for j in range(3, 9):
+                s = table.cell(i, j).text
+                worksheet.write(rows, cols, label=s)
+                cols += 1
+            worksheet.write(rows, cols, label="0")
+            worksheet.write(rows, cols + 1, label="0")
+            rows += 1
+        rows += 1
+        cols = 0
 
-workbook.save('C:\\Users\\王金宇\\Desktop\\数学建模\\111.xls')
+    end_table = tables[t - 1]
+    writeEndTable(end_table)
 
-'''
-            if i == 1:
-                result = result + " " + s
-            else:
-                result += s;
-            print(result)
-            '''
+   # for i in range(0, 10):
+       # print(worksheet.row(1)[0].value)
+
+    workbook.save(".\\CleanedData.xls")
+
+if __name__=="__main__":
+    main()
+
+
 
 
